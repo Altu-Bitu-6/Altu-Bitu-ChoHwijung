a4d1de1cec5d280bd4f9791d3bc2beba403a31a4
hvvijung <hvvijung@gmail.com>
2024-03-05 14:56:56 +0900

    [정수론] 3월 5일

--- 03_정수론/필수/2840.cpp
+++ 03_정수론/필수/2840.cpp
@@ -3,44 +3,61 @@
 
 using namespace std;
 
-
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
     int n, k;
+    int index = 0;
     bool is_circle = true;
-
+    
     cin >> n >> k;
 
     vector<string> circle(n);
 
-    for (int i; i<n; i++) circle[i] = "?";
+    for (int i = 0; i < n; i++)
+        circle[i] = "?";
 
-    for (int i; i<k; i++) {
+    for (int i = 0; i < k; i++) {
         int s;
         string letter;
         cin >> s >> letter;
 
-        if (circle[i] != "?") {
-            if (circle[i] == letter) continue;
+        index = (index + s ) % n; 
+
+        if (circle[index] != "?") {
+            if (circle[index] == letter) continue;
             is_circle = false;
         }
-        else circle[i] = letter;
+        else circle[index] = letter;
     }
 
-    for (int i; i<n; i++) {
-        if(circle[i] == "?") continue;
-        for (int j; j<n; j++) {
-            if (circle[i] == circle[j]){
+    for (int i = 0; i < n; i++) {
+        if (circle[i] == "?") continue;
+        for (int j = i + 1; j < n; j++) {
+            if (circle[i] == circle[j]) {
                 is_circle = false;
+                break;
             }
         }
     }
+    
+    int times = n;
 
-    if(is_circle) for (int i=0; i<n; i++) cout << circle[i];
-
-
+    if (is_circle) {
+        while (n--) {
+            if (index >= 0) {
+                cout << circle[index];
+                index--;
+            }
+            else {
+                index += times;
+                n++;
+            }
+       }
+    }
+    else cout << "!";
+    
+    return 0;
 }
